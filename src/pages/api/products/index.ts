6680b299fe42abcc3ad756269e6af73063adcff6
francomeriles-meli <franco.meriles@mercadolibre.com>
2023-01-10 14:50:23 -0300

    add handle error apis

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,17 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import service from '@services/bff'
-
-type Data = {
-  name: string
-}
+import { getErrorResponse } from '@utils/index'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  const response = await service.get(
-    `/products?condition=${req.query.condition}`
-  )
-  res.status(200).json({ ...response.data })
+  try {
+    const response = await service.get(
+      `/products?condition=${req.query.condition}`
+    )
+    res.status(200).json({ ...response.data })
+  } catch (err) {
+    res.status(err.response.status).json(getErrorResponse(err))
+  }
 }
