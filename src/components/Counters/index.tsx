32f2b102696ae0cdc89744fbf44563e6c2af17f6
francomeriles-meli <franco.meriles@mercadolibre.com>
2023-02-02 17:33:57 -0300

    fix scroll up

--- src/components/Counters/index.tsx
+++ src/components/Counters/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Text, Flex, Box, Heading } from '@chakra-ui/react'
 import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor'
 
 const Counters = () => {
   return (
@@ -23,7 +24,13 @@ const Counters = () => {
             m="0 20px"
           >
             <Text fontSize="7xl" fontWeight="bold" color="brand.500">
-              <CountUp end={4} separator="." enableScrollSpy />
+              <CountUp end={4} separator=".">
+                {({ countUpRef, start }) => (
+                  <VisibilitySensor onChange={start}>
+                    <span ref={countUpRef} />
+                  </VisibilitySensor>
+                )}
+              </CountUp>
             </Text>
             <Heading ml="4" fontSize="2xl" fontWeight="bold" color="white">
               Generaciones
@@ -36,7 +43,13 @@ const Counters = () => {
             m="0 20px"
           >
             <Text fontSize="7xl" fontWeight="bold" color="brand.500">
-              <CountUp end={112} separator="." enableScrollSpy />
+              <CountUp end={112} separator=".">
+                {({ countUpRef, start }) => (
+                  <VisibilitySensor onChange={start}>
+                    <span ref={countUpRef} />
+                  </VisibilitySensor>
+                )}
+              </CountUp>
             </Text>
             <Heading
               ml="4"
@@ -56,7 +69,13 @@ const Counters = () => {
             m="0 20px"
           >
             <Text fontSize="7xl" fontWeight="bold" color="brand.500">
-              <CountUp prefix="+" end={10000} separator="." enableScrollSpy />
+              <CountUp prefix="+" end={10000} separator=".">
+                {({ countUpRef, start }) => (
+                  <VisibilitySensor onChange={start}>
+                    <span ref={countUpRef} />
+                  </VisibilitySensor>
+                )}
+              </CountUp>
             </Text>
             <Heading
               ml="4"
